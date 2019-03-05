fe8d42ec99bc34d39e618f7b3c6e6fc02ce6d4f3
PaulPetroskiJr <PaulPetroskiJr@gmail.com>
2019-03-05 08:03:28 -0700

    Chpt9-Registration Page + Bootstrap

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   devServer: {
     port: 3000
+  },
+  configureWebpack: {
+    entry: {
+      app: "./src/main.js",
+      style: ["bootstrap/dist/css/bootstrap.min.css"]
+    }
   }
 };
