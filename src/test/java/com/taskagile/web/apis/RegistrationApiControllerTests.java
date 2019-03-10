0cb77dd76544dcb82ce3cb115e01149c50c39d43
PaulPetroskiJr <PaulPetroskiJr@gmail.com>
2019-03-09 17:58:35 -0700

    Chpt10-SecurityDeepDive
    Chapter 10 - Spring Security Deep Dive

--- src/test/java/com/taskagile/web/apis/RegistrationApiControllerTests.java
+++ src/test/java/com/taskagile/web/apis/RegistrationApiControllerTests.java
@@ -1,26 +1,31 @@
 package com.taskagile.web.apis;
 
-import com.taskagile.domain.application.UserService;
-import com.taskagile.domain.model.user.EmailAddressExistsException;
-import com.taskagile.domain.model.user.UsernameExistsException;
-import com.taskagile.utils.JsonUtils;
-import com.taskagile.web.payload.RegistrationPayload;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.taskagile.config.SecurityConfiguration;
+import com.taskagile.domain.application.UserService;
+import com.taskagile.domain.model.user.EmailAddressExistsException;
+import com.taskagile.domain.model.user.UsernameExistsException;
+import com.taskagile.utils.JsonUtils;
+import com.taskagile.web.payload.RegistrationPayload;
+import com.taskagile.web.apis.RegistrationApiController;
+
 @RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {SecurityConfiguration.class, RegistrationApiController.class})
 @WebMvcTest(RegistrationApiController.class)
 public class RegistrationApiControllerTests {
 
