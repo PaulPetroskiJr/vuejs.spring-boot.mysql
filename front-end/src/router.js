611d0648abb360da14e05a0181e1b19ee4408e76
PaulPetroskiJr <PaulPetroskiJr@gmail.com>
2019-03-13 18:33:45 -0400

    Chpt11-StateMgmg
    Setting up Vuex for homepage

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -1,8 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
-import LoginPage from "@/views/LoginPage";
 import HomePage from "@/views/HomePage";
+import LoginPage from "@/views/LoginPage";
 import RegisterPage from "@/views/RegisterPage";
+import BoardPage from "@/views/BoardPage";
 
 Vue.use(Router);
 
@@ -24,6 +25,11 @@ export default new Router({
       path: "/register",
       name: "register",
       component: RegisterPage
+    },
+    {
+      path: "/board/:boardId",
+      name: "board",
+      component: BoardPage
     }
   ]
 });
